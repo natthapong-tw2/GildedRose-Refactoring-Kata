0420bf2bfcaeed84d8c7bf449774ff0bdc544e0a
wittakarn-tw <x.keeratichayakorn@jetstar.com>
2022-05-13 12:02:27 +0700

    Add one more test case for the first if statement(WIP)

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -4,6 +4,13 @@ describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
+  });
+
+  it('should reduce quantity when name is foo ', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('foo');
+    expect(0).toBe(gildedRose.items[0].quality);
   });
 });
