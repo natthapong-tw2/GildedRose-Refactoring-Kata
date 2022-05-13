11d8f26a075f020388e418ceb2b4e2a1baae0f78
pee-tw <pee.tankulrat@jetstar.com>
2022-05-13 11:39:17 +0700

    First block reduce quality test

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,16 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+describe("Gilded Rose", function () {
+  it("should foo", function () {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
+
+  it('should reduce the quality, when name is not equal to (Aged Brie) or (Backstage passes to a TAFKAL80ETC concert) or (Sulfuras, Hand of Ragnaros)', () => {
+    const item = new Item("foo", 0, 1)
+    const gildedRose = new Shop([item]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  })
 });
