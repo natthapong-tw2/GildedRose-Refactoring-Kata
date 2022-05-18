37b0b3dc33718286a0df69f9200fa20eab7f8f80
eveemt <monpriya.thoughtworks.com>
2022-05-18 14:30:25 +0700

    add test reduce sellIn if name not equal surlfuras

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,28 +1,35 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
-const agedBrie = 'Aged Brie'
-const backStage = 'Backstage passes to a TAFKAL80ETC concert'
-const sulfuras = 'Sulfuras, Hand of Ragnaros'
+const agedBrie = "Aged Brie";
+const backStage = "Backstage passes to a TAFKAL80ETC concert";
+const sulfuras = "Sulfuras, Hand of Ragnaros";
 
 describe("Gilded Rose", function () {
-  describe('First if block', () => {
+  describe("First if block", () => {
     it("should foo", function () {
       const gildedRose = new Shop([new Item("foo", 0, 0)]);
       const items = gildedRose.updateQuality();
       expect(items[0].name).toBe("foo");
     });
-    it('should reduce the quality, when name is not equal to (Aged Brie) or (Backstage passes to a TAFKAL80ETC concert) or (Sulfuras, Hand of Ragnaros)', () => {
-      const item = new Item("foo", 0, 1)
+    it("should reduce the quality, when name is not equal to (Aged Brie) or (Backstage passes to a TAFKAL80ETC concert) or (Sulfuras, Hand of Ragnaros)", () => {
+      const item = new Item("foo", 0, 1);
       const gildedRose = new Shop([item]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(0);
-    })
-    it('should not reduce the quality, when the name is sulfuras', ()=> {
-      const item = new Item(sulfuras, 0, 1)
-      
+    });
+    it("should not reduce the quality, when the name is sulfuras", () => {
+      const item = new Item(sulfuras, 0, 1);
+
       const gildedRose = new Shop([item]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(1);
-    })
-  })
+    });
+    it("should reduce the sellIn, when the name not equal sulfuras", () => {
+      const item = new Item("bar", 0, 0);
+
+      const gildedRose = new Shop([item]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+    });
+  });
 });
