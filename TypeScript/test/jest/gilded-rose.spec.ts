32ce936e3982b275c0a975a345441c726b373db7
Meng Lu <meng.lu@jetstar.com>
2022-05-19 17:54:12 +1000

    add test cases

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -6,4 +6,22 @@ describe('Gilded Rose', () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
   });
+
+  it('quality should be minus 1 when name is not "Aged Brie" or "Backstage passes to a TAFKAL80ETC concert" or "Sulfuras, Hand of Ragnaros"', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it('quality should be add 2 when name is "Backstage passes to a TAFKAL80ETC concert and sellIn less than 11, greater than 6 and quality less than 50"', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 7, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(3);
+  });
+
+  it('quality should be add 3 when name is "Backstage passes to a TAFKAL80ETC concert and sellIn less than 6 and quality less than 50"', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(4);
+  });
 });
