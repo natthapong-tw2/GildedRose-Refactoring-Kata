58ce144ab5d970ec3ef5194a58bdd81f1877f4ea
wweiyang <nwy290@gmail.com>
2022-05-19 14:44:06 +0800

    Added more tests

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -4,6 +4,94 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
   });
 });
+
+describe("Sulfuras never changes in quality", function() {
+  it("Sulfuras quality should remain the same", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+  });
+});
+
+describe("Aged Brie increases in quality as sellIn decreases", function() {
+  it("Aged Brie quality should +1, if quality < 50, sellIn > 0", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(11);
+  })
+  it("Aged Brie quality should +2, if quality < 50, sellIn <= 0", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(12);
+  })
+})
+
+describe("Backstage passes quality changes based on sellIn", function() {
+  it("Backstage passes quality should +1, if quality < 50, sellIn >= 11", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 11, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(11);
+  });
+  it("Backstage passes quality should +2, if quality < 50, sellIn < 11", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(12);
+  });
+  it("Backstage passes quality should +3, if quality < 50, sellIn < 6", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(13);
+  });
+  it("Backstage passes quality should be 0, if quality < 50, sellIn < 0", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+});
+
+describe("All other items quality changes based on sellIn", function() {
+  it("All other items quality should -1, if quality < 50, sellIn > 0", function() {
+    const gildedRose = new Shop([new Item("foo", 1, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(9);
+  });
+  it("All other items quality should -2, if quality < 50, sellIn <= 0", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(8);
+  });
+});
+
+describe("Items quality is never negative", function() {
+  it("Aged Brie quality is never negative", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(2);
+  });
+  it("Backstage passes quality is never negative", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+  it("All other items quality is never negative", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+});
+
+
+// filter by name, then quality, then sellIn
+
+  // Sulfuras -> quality always 80, no need to update
+  // Aged Brie -> if quality < 50, sellIn >= 0, quality +1
+  //                               sellIn < 0, quality +2
+  // Backstage passes -> if quality < 50, sellIn < 11, quality +2
+  //                                      sellIn < 6, quality +3
+  //                                      sellIn < 0 , quality = 0
+  // All other items -> if quality > 0, quality -1
+  //                                    sellIn < 0, quality -2
+  // All items -> quality always >= 0
