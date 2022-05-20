4f490da56de7a5d18348b308c59261c840e1331f
Daniel Sutton <danielsuttonp@gmail.com>
2022-05-20 14:48:14 +1000

    Add tests for Backstage Passes item

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -31,6 +31,11 @@ describe("Shop", function() {
       const gildedRose = new Shop([new Item("foo", 0, 0)]);
       expect(() => gildedRose.updateQuality()).not.toThrow();
     });
+
+    it("should not throw when run 'updateQuality' with no items in the store", function() {
+      const gildedRose = new Shop();
+      expect(() => gildedRose.updateQuality()).not.toThrow();
+    });
   });
 
   describe("Regular Item", () => {
@@ -81,14 +86,13 @@ describe("Shop", function() {
       expect(items[0].quality).toBe(1);
     });
 
-    // TODO: change test name 
     it("'quality' should increase by 2 even when 'sellIn' < 0", function() {
       const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
       gildedRose.updateQuality();
       gildedRose.updateQuality();
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toBe(-3);
-      expect(items[0].quality).toBe(3);
+      expect(items[0].quality).toBe(6);
     });
 
     it("'quality' should not increase above 50", function() {
@@ -98,5 +102,85 @@ describe("Shop", function() {
       expect(items[0].sellIn).toBe(-1);
       expect(items[0].quality).toBe(50);
     });
+  });
+
+  describe("Backstage Passes", () => {
+    it("'quality' should increase by 1 if sellIn is 11", function() {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 11, 0)
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(10);
+      expect(items[0].quality).toBe(1);
+    });
+
+    it("'quality' should increase by 2 if 'sellIn' is 10", function() {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0)
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(9);
+      expect(items[0].quality).toBe(2);
+    });
+
+    it("'quality' should increase by 2 each day if 'sellIn' is <= 10 AND > 5", function() {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0)
+      ]);
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(5);
+      expect(items[0].quality).toBe(10);
+    });
+
+    it("'quality' should increase by 3 if 'sellIn' is 5", function() {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0)
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(4);
+      expect(items[0].quality).toBe(3);
+    });
+
+    it("'quality' should increase by 3 each day if 'sellIn' is <= 5 AND >= 0", function() {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0)
+      ]);
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(0);
+      expect(items[0].quality).toBe(15);
+    });
+
+    it("'quality' should always be 0 when 'sellIn' < 0", function() {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+      expect(items[0].quality).toBe(0);
+    });
+
+    // it("'quality' should always be ('sellIn' - 10) + 10 + 15 when 'sellIn' reaches 0", function() {
+    //   const sellIn = 100;
+    //   const items = Array(sellIn)
+    //     .fill(null)
+    //     .reduce(
+    //       gildedRose => gildedRose.updateQuality(),
+    //       new Shop([
+    //         new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, 0)
+    //       ])
+    //     );
+
+    //   expect(items[0].sellIn).toBe(0);
+    //   expect(items[0].quality).toBe(115);
+    // });
+    //
   });
 });
