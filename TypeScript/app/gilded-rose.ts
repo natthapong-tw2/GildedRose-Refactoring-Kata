5d0ae47d8088dedb22f8305cc68df381b9965cbb
Natthapong Intharak <natthapong.intharak+jetstar@thoughtworks.com>
2022-05-25 09:20:35 +0700

    Extract sulfuras from other item

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -53,17 +53,15 @@ export class GildedRose {
             }
           }
         }
+      } else if (item.name == ItemName.Sulfuras) {
+
       } else {
         if (item.quality > 0) {
-          if (item.name != ItemName.Sulfuras) {
-            item.quality = item.quality - 1
-          }
+          item.quality = item.quality - 1
         }
         if(item.sellIn < 0) {
           if (item.quality > 0) {
-            if (item.name != ItemName.Sulfuras) {
-              item.quality = item.quality - 1
-            }
+            item.quality = item.quality - 1
           }
         }
       }
