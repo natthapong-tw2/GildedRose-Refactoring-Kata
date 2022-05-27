7e2c44d4c25d00e5413db2aa4ce9649badd24cb8
mew <mewchildy@gmail.com>
2022-05-27 10:18:57 +0700

    Refactor to use test.each to handle test cases

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,4 +1,4 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
   it("should foo", function() {
@@ -35,16 +35,15 @@ describe("Aged brie", () => {
 })
 
 describe("Sulfuras, Hand of Ragnaros", () => {
-  [
-    { name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 1},
-    { name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 50},
-    { name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 51},
-  ].forEach(item => {
-    it('should never be sold or decrease in quality', () => {
+  test.each( 
+    [
+      { name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 1},
+      { name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 50},
+      { name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 51},
+  ])('should never be sold or decrease in quality', item => {
       const sulfuras = new Shop([item]);
       const items = sulfuras.updateQuality();
       expect(items[0]).toEqual({ name: "Sulfuras, Hand of Ragnaros", sellIn: item.sellIn, quality: item.quality});
-    })
   })
 })
 
@@ -55,16 +54,14 @@ describe("Backstage passes to a TAFKAL80ETC concert", () => {
     expect(items[0]).toEqual({ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 4, quality: 3});
   });
 
-  [
+  test.each([
     { name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 8, quality: 4},
     { name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 6, quality: 8},
-    { name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 0},
-  ].forEach(item => {
-    it("should increase quality by two as the date has approaches 10 days or less", () => {
+    { name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 0}
+  ])('should increase quality by two as the date has approaches 10 days or less', item => {
       const backstage = new Shop([new Item(item.name, item.sellIn, item.quality)]);
       const items = backstage.updateQuality();
       expect(items[0]).toEqual({ name: item.name, sellIn: item.sellIn - 1, quality: item.quality + 2});
-    })
   })
 
   it("should have zero quality if the performance already end", () => {
